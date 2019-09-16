d0883264ae62ffebf7f9b2b1d366f0487277492b
Yuki Sawa <yuki.sawa@gotinder.com>
2019-09-16 10:15:54 -0700

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ysawa0/pre-commit-golang
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+
+import (
+	"fmt"
+	"sync"
+	//_ "sync"/
+)
+
+var x = 0
+func adder() {
+	x = x + 1
+}
+
+func main() {
+	fmt.Println("asd")
+	//x := 0
+	wg := sync.WaitGroup{}
+	m := sync.RWMutex{}
+	for i:=0 ; i<100 ; i++ {
+		wg.Add(1)
+		go func() {
+			m.Lock()
+			adder()
+			wg.Done()
+			m.Unlock()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(x)
+}
