b499c1753621b80f1fd7d90e614244bf5c4ab45c
Yuki Sawa <yuki.sawa@gotinder.com>
2019-09-16 10:25:13 -0700

    Update

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"sync"
 
+
+
+	
 )
 
 var x = 0
--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -1,24 +1,3 @@
-# #!/usr/bin/env bash
-# echo "ASDF"
-# echo $GIT_DIR
-# GIT_DIR=$PWD
-# echo $GIT_DIR
-# FILES=$(go list ./...  | grep -v /vendor/)
-
-# go test -tags=unit -timeout 30s -short -v ${FILES}
-
-# go test ./...
-
-# returncode=$?
-# if [ $returncode -ne 0 ]; then
-#   echo "unit tests failed"
-#   exit 1
-# fi
-
-# /usr/local/bin/gofmt
-# -w -s $FilePath$
-# $FilePath$
-
 echo "ASDF"
 echo "ASDF"
 echo "ASDF"
